3a372bf95d963b5095b109d9575ce09f9a4ea308
Rhuan C. Borges <97767859+rhuancborges@users.noreply.github.com>
2022-08-17 11:18:23 -0300

    Update preco_alcool.cpp

--- preco_alcool.cpp
+++ preco_alcool.cpp
@@ -1,3 +1,8 @@
+/* Programa que calcula o preço a ser pago pela quantidade de álcool colocado no veículo:
+	- Preço do litro: R$2,90;
+	- Desconto para quantidades menores que 20 litros: 3%;
+	- Desconto para quantidades maiores que 20 litros: 5%. */
+
 #include <iostream>
 using namespace std;
 
